f9ab78e3ab83acad0dd99dc2b7147907989ab352
danazsdiq <danazsdiq17@gmail.com>
2023-08-16 22:46:48 +0300

    feat: Increasing payload size limit

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { organizationsRouter } from "./controllers/organizations";
 
 const app: Express = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 app.use("/opportunities", opportunitiesRouter);
 app.use("/organizations", organizationsRouter);
