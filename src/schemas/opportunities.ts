f1c949998abe669943507e1220951ae75ac4bdcd
danazsdiq <danazsdiq17@gmail.com>
2023-08-16 20:27:14 +0300

    fix: Adding new columns to opportunity schema

--- src/schemas/opportunities.ts
+++ src/schemas/opportunities.ts
@@ -7,11 +7,17 @@ const opportunitySchema = z.object({
   description: z.string(),
   labels: z.array(z.string()),
   commitment: z.string(),
-  url: z.string().url()
+  url: z.string().url(),
+
+  created_at: z.coerce.date(),
+  updated_at: z.coerce.date(),
+  deleted_at: z.coerce.date() || z.null()
 });
 
 export type Opportunity = z.infer<typeof opportunitySchema>;
 
-const createOpportunitiesSchema = z.array(opportunitySchema.omit({ id: true }));
+const createOpportunitiesSchema = z.array(
+  opportunitySchema.omit({ id: true, updated_at: true, deleted_at: true })
+);
 export const parseCreateOpportunitiesData = (opportunities: Opportunity[]) =>
   createOpportunitiesSchema.safeParse(opportunities);
